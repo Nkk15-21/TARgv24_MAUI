b330ab94c4e7d2295ce47966b50914bf6fbfc92c
Nkk15-21 <nkk.sith@gmail.com>
2025-09-16 10:34:36 +0300

    +

--- DateTimePage.xaml.cs
+++ DateTimePage.xaml.cs
@@ -1,4 +1,7 @@
-using Microsoft.Maui.Layouts;
+using Microsoft.Maui.Controls;  // For controls like Label, Picker, DatePicker, TimePicker, etc.
+using Microsoft.Maui.Graphics;  // For handling colors and graphics
+using Microsoft.Maui.Layouts;  // For AbsoluteLayout and other layout elements
+
 
 namespace TARgv24;
 
@@ -7,6 +10,7 @@ public partial class DateTimePage : ContentPage
 	Label mis_on_valitud;
 	DatePicker datePicker;
 	TimePicker timePicker;
+	Picker picker;
 	AbsoluteLayout al;
     public DateTimePage()
 	{
@@ -29,6 +33,37 @@ public partial class DateTimePage : ContentPage
 			Format="D"
 		};
         datePicker.DateSelected += Kuupäeva_valimine;
+		timePicker = new TimePicker
+		{
+			BackgroundColor = Color.FromRgb(200,200,100),
+			TextColor = Colors.Black,
+			FontFamily = "Lovin Kites 400",
+			Time = new TimeSpan(12,0,0),
+			Format="T"
+
+		};
+		timePicker.PropertyChanged += (s, e) =>
+		{
+			if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
+			{
+				mis_on_valitud.Text = $"Valisite kellaaja: {timePicker.Time}";
+			}
+		};
+
+		picker = new Picker
+		{
+			Title = "Vali üks"
+		};
+		picker.Items.Add("Kuus");
+		picker.SelectedIndexChanged += (s, e) =>
+		{
+			if (picker.SelectedIndex != -1)
+			{
+				mis_on_valitud.Text = $"Valisite: {picker} "
+			}
+		};
+
+
 		al = new AbsoluteLayout { Children = { mis_on_valitud, datePicker } };
 		AbsoluteLayout.SetLayoutBounds(mis_on_valitud, new Rect(0.5, 0.2, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
 		AbsoluteLayout.SetLayoutFlags(mis_on_valitud, AbsoluteLayoutFlags.All);
